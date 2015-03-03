e10518fc9253184c0af64719501d2b39954ed3cc
vwu <vwu@grubhub.com>
2015-03-02 22:09:10 -0600

    Update Mongoose dependency

--- services/models/mongoConnect.js
+++ services/models/mongoConnect.js
@@ -3,8 +3,6 @@ var mongoose = require('mongoose');
 var localUriString = 'mongodb://localhost/yadaapp';
 var herokuUriString = 'mongodb://yadaapp:ecclesiastesfrancisautomobilesneakers@ds049181.mongolab.com:49181/heroku_app34490307';
 
-
-
 var uriString = localUriString;
 mongoose.connect(uriString, function (error) {
   if (!error) {
