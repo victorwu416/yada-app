66c5410cd907112005e38d3ad064789c4438d7ad
vwu <vwu@grubhub.com>
2015-03-08 14:50:13 -0500

    Add again

--- services/models/mongoConnect.js
+++ services/models/mongoConnect.js
@@ -8,7 +8,8 @@ mongoose.connect(localUriString, function (error) {
   if (!error) {
     console.log('Successfully connected to: ' + localUriString);
   } else {
-    console.log('Error connecting to: ' + localUriString + ': ' + error);
+    console.log('Error connecting to: ' + localUriString + ': ' + error + ': ' 
+                + 'Attempting to connect to MongoLab MongoDB');
 		
     mongoose.connect(herokuUriString, function (error) {		
       if (!error) {
